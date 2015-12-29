1d1157e0a8bdae9988d35733452d39a30426f36b
joserobjr <jose.rob.jr@gmail.com>
2015-12-29 13:17:42 -0300

    Added options to replace battery recipes and disable chargepads recipes

--- src/main/java/br/com/gamemods/ic2/charger/ChargerMod.java
+++ src/main/java/br/com/gamemods/ic2/charger/ChargerMod.java
@@ -4,6 +4,8 @@ import br.com.gamemods.ic2.charger.proxy.CommonProxy;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.common.config.Configuration;
 
 @Mod(modid = "IC2Charger", name = "IC2 Charger", version = "1.0.0",
     dependencies = "required-after:IC2")
@@ -17,10 +19,17 @@ public class ChargerMod
     public static ChargerMod instance;
 
     @Mod.EventHandler
+    public void onPreInit(FMLPreInitializationEvent event)
+    {
+        proxy.loadConfig(new Configuration(event.getSuggestedConfigurationFile()));
+    }
+
+    @Mod.EventHandler
     public void onInit(FMLInitializationEvent event)
     {
         proxy.registerBlocks();
         proxy.registerTiles();
         proxy.registerNetwork(this);
+        proxy.registerRecipes();
     }
 }
--- src/main/java/br/com/gamemods/ic2/charger/proxy/CommonProxy.java
+++ src/main/java/br/com/gamemods/ic2/charger/proxy/CommonProxy.java
@@ -5,10 +5,21 @@ import br.com.gamemods.ic2.charger.GuiHandler;
 import br.com.gamemods.ic2.charger.charger.*;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
+import ic2.core.AdvRecipe;
+import ic2.core.Ic2Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
+
+import java.util.Iterator;
 
 public class CommonProxy
 {
     public BlockCharger blockCharger;
+    public byte configs;
 
     public void registerBlocks()
     {
@@ -26,5 +37,53 @@ public class CommonProxy
     public void registerNetwork(ChargerMod mod)
     {
         NetworkRegistry.INSTANCE.registerGuiHandler(mod, new GuiHandler());
+    }
+
+    public void registerRecipes()
+    {
+        boolean replace = (configs & 1) > 0;
+        boolean remove = (configs & 1<<1) > 0;
+        if(configs > 0)
+        {
+            @SuppressWarnings("unchecked")
+            Iterator<IRecipe> recipes = CraftingManager.getInstance().getRecipeList().iterator();
+            while (recipes.hasNext())
+            {
+                IRecipe recipe = recipes.next();
+                if(recipe instanceof AdvRecipe)
+                {
+                    AdvRecipe advRecipe = (AdvRecipe) recipe;
+                    ItemStack recipeOutput = advRecipe.getRecipeOutput();
+                    if(recipeOutput.getItem() == Ic2Items.batBox.getItem())
+                    {
+                        switch (recipeOutput.getItemDamage())
+                        {
+                            case 0:case 1:case 2:case 7:
+                                if(replace)
+                                    recipeOutput.func_150996_a(Item.getItemFromBlock(blockCharger));
+                            break;
+                        }
+                    }
+                    else if(remove && recipeOutput.getItem() == Ic2Items.ChargepadbatBox.getItem())
+                    {
+                        recipes.remove();
+                    }
+                }
+            }
+        }
+    }
+
+    public void loadConfig(Configuration config)
+    {
+        config.load();
+        Property prop = config.get("recipe", "replace-recipes", true);
+        prop.comment = "Set to true to replace BatBox, CESU, MFE and MFSU recipes";
+        configs |= prop.getBoolean()? 1 : 0;
+
+        prop = config.get("recipe", "disable-chargepads-recipes", true);
+        prop.comment = "Set tto true to disable all chargepads recipes";
+        configs |= prop.getBoolean()? 1<<1 : 0;
+
+        config.save();
     }
 }
