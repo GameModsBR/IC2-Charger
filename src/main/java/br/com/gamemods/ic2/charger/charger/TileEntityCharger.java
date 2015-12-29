17a693c66d098d7c7a6ab10c0db58f4084f663c3
joserobjr <jose.rob.jr@gmail.com>
2015-12-29 09:33:59 -0300

    Made charger accept energy from IC2 wires

--- src/main/java/br/com/gamemods/ic2/charger/charger/TileEntityCharger.java
+++ src/main/java/br/com/gamemods/ic2/charger/charger/TileEntityCharger.java
@@ -1,5 +1,8 @@
 package br.com.gamemods.ic2.charger.charger;
 
+import ic2.api.energy.event.EnergyTileLoadEvent;
+import ic2.api.energy.event.EnergyTileUnloadEvent;
+import ic2.api.energy.tile.IEnergySink;
 import ic2.core.block.TileEntityInventory;
 import ic2.core.block.invslot.InvSlot;
 import ic2.core.block.invslot.InvSlotCharge;
@@ -8,8 +11,11 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.util.ForgeDirection;
 
-public abstract class TileEntityCharger extends TileEntityInventory
+public abstract class TileEntityCharger extends TileEntityInventory implements IEnergySink
 {
     public final InvSlotCharge chargeSlot;
     public final InvSlotDischarge dischargeSlot;
@@ -17,11 +23,14 @@ public abstract class TileEntityCharger extends TileEntityInventory
     public double energy;
     public double maxStorage;
 
+    public int tier;
+
     public TileEntityCharger(int tier, int output, double maxStorage)
     {
         this.maxStorage = maxStorage;
         chargeSlot = new InvSlotCharge(this, 0, tier);
         dischargeSlot = new InvSlotDischarge(this, 1, InvSlot.Access.IO, tier, InvSlot.InvSide.BOTTOM);
+        this.tier = tier;
     }
 
     @Override
@@ -51,6 +60,56 @@ public abstract class TileEntityCharger extends TileEntityInventory
             if(energyBefore != energy)
                 worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
         }
+    }
+
+    @Override
+    public void onLoaded()
+    {
+        super.onLoaded();
+        if(!worldObj.isRemote)
+            MinecraftForge.EVENT_BUS.post(new EnergyTileLoadEvent(this));
+    }
+
+    @Override
+    public void onUnloaded()
+    {
+        super.onUnloaded();
+        if(!worldObj.isRemote)
+            MinecraftForge.EVENT_BUS.post(new EnergyTileUnloadEvent(this));
+    }
+
+    @Override
+    public boolean acceptsEnergyFrom(TileEntity tileEntity, ForgeDirection forgeDirection)
+    {
+        return true;
+    }
+
+    @Override
+    public double injectEnergy(ForgeDirection directionFrom, double amount, double voltage)
+    {
+        if(energy >= maxStorage)
+            return amount;
+
+        double increment = Math.max(Math.min(maxStorage - energy, amount), 0);
+        energy += increment;
+        if(increment > 0)
+        {
+            markDirty();
+            worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
+        }
+        return amount - increment;
+    }
+
+    @Override
+    public int getSinkTier()
+    {
+        return tier;
+    }
+
+    @Override
+    public double getDemandedEnergy()
+    {
+        return maxStorage - energy;
     }
 
     public float getChargeLevel()
